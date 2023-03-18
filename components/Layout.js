159055875f30e15024aa882a5879b43836a250fd
t1k2a <maxtj1122@gmail.com>
2023-03-18 22:11:42 +0900

    all done

---
+++ components/Layout.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import styles from "./layout.module.css"
+import utilStyles from "../styles/utils.module.css"
+import Link from "next/link";
+
+const name = "Shin Code"
+export const siteTitle = "Next.js blog"
+
+function Layout({ children, home }) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <img src="/images/profile.png" className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`} />
+                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <img src="/images/profile.png" className={`${utilStyles.borderCircle}`} />
+                    </>
+                )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div>
+                    <Link href='/'>← ホームへ戻る</Link>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export async function getStaticProps(ctx){
+
+
+    return {
+        props:{
+            data:null
+        }
+    }
+}
+
+export default Layout;
---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function Custom404() {
+    return (
+        <h1>
+            Not Found
+        </h1>
+    );
+}
