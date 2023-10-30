e21a4589c5fe66f26f953c6671330239dbae6f3a
t1k2a <maxtj1122@gmail.com>
2023-10-30 13:21:51 +0000

    実績の一部を削除

--- components/Works.js
+++ components/Works.js
@@ -1,10 +1,10 @@
-import Image from 'next/image'
+import React from 'react'
+
 import styles from '../styles/Home.module.css'
-import Link from "next/link";
 import utilStyle from '../styles/utils.module.css'
 
 function Works() {
-    return (
+  return (
         <section>
         <h2>WORKS</h2>
         <div  className={styles.grid}>
@@ -13,16 +13,6 @@ function Works() {
               うつぺでぃあ（閉鎖中）
             </p>
             <img src="https://t1k2a.github.io/about_me/img/utsupedia.png"></img>
-          </div>
-          <div>
-            <p className={utilStyle.headingMd}>エミナルグループ総合サイト</p>
-            <Link href='http://eminal-group.jp/#/' target="blank">
-              <Image 
-                width={600}
-                height={300}
-                src="/images/eminal_group.png"
-              />
-            </Link>
           </div>
         </div>
         
